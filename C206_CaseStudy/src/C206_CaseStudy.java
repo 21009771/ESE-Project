a20b043e3073cfb74d0c9ba481d81e5fd755c84c
TOSHIBA <21009771@myrp.edu.sg>
2022-08-04 11:28:29 +0800

    Menu

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,7 +3,21 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
 
 	}
+
+}
+
+public static void menu() {
+	ResourceCentre.setHeader("RESOURCE CENTRE APP");
+	System.out.println("1. Display Inventory");
+	System.out.println("2. Add item");
+	System.out.println("3. Loan item");
+	System.out.println("4. Return item");
+	System.out.println("5. Quit");
+	Helper.line(80, "-");
 
 }
