b76042a11a7ef41cde2ec9cc712e1d6ffee5c580
Ardilla <21036553@myrp.edu.sg>
2022-08-04 14:05:34 +0800

    commit

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -8,16 +8,6 @@ public class C206_CaseStudy {
 		
 
 	}
-
 }
 
-public static void menu() {
-	ResourceCentre.setHeader("RESOURCE CENTRE APP");
-	System.out.println("1. Display Inventory");
-	System.out.println("2. Add item");
-	System.out.println("3. Loan item");
-	System.out.println("4. Return item");
-	System.out.println("5. Quit");
-	Helper.line(80, "-");
 
-}
