568871014d5c91bfc8597c8ca0a82f5d24d22474
shhhenru <21034608@myrp.edu.sg>
2022-08-05 21:36:45 +0800

    option menu

--- C206_CaseStudy/src/OptionMenu.java
+++ C206_CaseStudy/src/OptionMenu.java
@@ -14,5 +14,33 @@
  *
  */
 public class OptionMenu {
+	
+	public static void menu() {
+		TuitionManagement.setHeader("TUITION MANAGEMENT APP");
+		System.out.println("1. Display all");
+		System.out.println("2. Add enquiry ");
+		System.out.println("3. Delete Enquiry");
+		System.out.println("4. Edit Enquiry");
+		System.out.println("5. Quit");
+		Helper.line(80, "-");
+
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
+	}
+
+	public static String showAvailability(boolean isAvailable) {
+		String avail;
+
+		if (isAvailable == true) {
+			avail = "Yes";
+		} else {
+			avail = "No";
+		}
+		return avail;
+	}
 
 }
